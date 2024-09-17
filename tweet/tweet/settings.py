a7a382aaff5f85e499c597f058069fd85d14f385
Dhiraj2060 <dhirajsharma2060@gmail.com>
2024-09-17 16:26:31 +0530

    final task for get password and search

--- tweet/tweet/settings.py
+++ tweet/tweet/settings.py
@@ -147,10 +147,10 @@ LOGOUT_REDIRECT_URL='/tweetup/'
 
 
 # Email Backend configuration (Gmail example)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.getenv('USER')  # Your email address
 EMAIL_HOST_PASSWORD = os.getenv('HOST_PASSWORD')  # Your email password (use environment variables for security in production)
 
